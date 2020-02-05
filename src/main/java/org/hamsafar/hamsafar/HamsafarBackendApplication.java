93dd6ad361a44975610d698d7e3b68c10b376d33
Farhad Farhadi <farhadf4rh4di@gmail.com>
2020-02-05 20:05:51 +0330

    enabled mongo auditing

--- src/main/java/org/hamsafar/hamsafar/HamsafarBackendApplication.java
+++ src/main/java/org/hamsafar/hamsafar/HamsafarBackendApplication.java
@@ -2,12 +2,12 @@ package org.hamsafar.hamsafar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
+@EnableMongoAuditing
 @SpringBootApplication
 public class HamsafarBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HamsafarBackendApplication.class, args);
     }
-
 }
