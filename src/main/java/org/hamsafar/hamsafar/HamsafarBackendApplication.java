ac64c881483314879a29f679a668c8a32c8256bb
Farhad Farhadi <farhadf4rh4di@gmail.com>
2020-02-05 18:55:48 +0330

    added initial project files from spring initializer

---
+++ src/main/java/org/hamsafar/hamsafar/HamsafarBackendApplication.java
@@ -0,0 +1,13 @@
+package org.hamsafar.hamsafar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamsafarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HamsafarBackendApplication.class, args);
+    }
+
+}
