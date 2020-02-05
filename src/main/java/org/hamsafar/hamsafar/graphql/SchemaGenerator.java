13a9cb6918718be6f2c279a8bc114b00310d08e4
Farhad Farhadi <farhadf4rh4di@gmail.com>
2020-02-05 22:42:09 +0330

    added city service to SchemaGenerator

--- src/main/java/org/hamsafar/hamsafar/graphql/SchemaGenerator.java
+++ src/main/java/org/hamsafar/hamsafar/graphql/SchemaGenerator.java
@@ -7,6 +7,7 @@ import io.leangen.graphql.metadata.strategy.query.AnnotatedResolverBuilder;
 import io.leangen.graphql.metadata.strategy.value.jackson.JacksonValueMapperFactory;
 import lombok.AllArgsConstructor;
 import org.hamsafar.hamsafar.graphql.services.AdminService;
+import org.hamsafar.hamsafar.graphql.services.CityService;
 import org.hamsafar.hamsafar.graphql.services.auth.AdminAuth;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
@@ -17,12 +18,14 @@ public class SchemaGenerator {
     private final AdminService adminService;
     private final AdminAuth adminAuth;
 
+    private final CityService cityService;
+
     @Bean
     public GraphQL getGraphQL() {
         GraphQLSchema schema = new GraphQLSchemaGenerator()
                 .withResolverBuilders(
                         new AnnotatedResolverBuilder())
-                .withOperationsFromSingletons(adminService, adminAuth)
+                .withOperationsFromSingletons(adminService, adminAuth, cityService)
                 .withValueMapperFactory(new JacksonValueMapperFactory())
                 .generate();
         return GraphQL.newGraphQL(schema).build();
