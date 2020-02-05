5b7c825180bae7c0b5a19e403ea4a496023595cc
Farhad Farhadi <farhadf4rh4di@gmail.com>
2020-02-05 21:44:04 +0330

    added search method to city repository

--- src/main/java/org/hamsafar/hamsafar/repository/CityRepository.java
+++ src/main/java/org/hamsafar/hamsafar/repository/CityRepository.java
@@ -7,9 +7,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CityRepository extends MongoRepository<City, String> {
+    Optional<City> findByNamesContains(List<String> names);
+
     List<City> findAllByVerifiedTrue();
 
     Page<City> findAllByVerified(Boolean isVerified, Pageable pageable);
