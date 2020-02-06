f485fc5ad6f313d866afa7e32271d83798d15835
Farhad Farhadi <farhadf4rh4di@gmail.com>
2020-02-06 16:11:34 +0330

    optimized the tag services

--- src/main/java/org/hamsafar/hamsafar/graphql/services/TagService.java
+++ src/main/java/org/hamsafar/hamsafar/graphql/services/TagService.java
@@ -1,6 +1,5 @@
 package org.hamsafar.hamsafar.graphql.services;
 
-import io.leangen.graphql.annotations.GraphQLMutation;
 import io.leangen.graphql.annotations.GraphQLNonNull;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
@@ -11,7 +10,6 @@ import org.hamsafar.hamsafar.repository.TagRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Optional;
 
@@ -38,11 +36,13 @@ public class TagService {
         return tagOptional.get();
     }
 
-    @GraphQLMutation
-    public Tag addTag(@GraphQLNonNull String title) {
-        return this.tagRepository.save(Tag.builder()
-                .title(title)
-                .places(new LinkedHashSet<>())
-                .build());
+    @GraphQLQuery
+    public Tag getTagByTitle(@GraphQLNonNull String title) {
+        Optional<Tag> tagOptional = this.tagRepository.findByTitleAndVerifiedTrue(title);
+        if (tagOptional.isEmpty()) {
+            log.error("Invalid Tag Title");
+            throw new RuntimeException("Invalid Tag Title, Not Found");
+        }
+        return tagOptional.get();
     }
 }
---
+++ src/main/java/org/hamsafar/hamsafar/graphql/services/adjust/TagAdjust.java
@@ -0,0 +1,30 @@
+package org.hamsafar.hamsafar.graphql.services.adjust;
+
+import io.leangen.graphql.annotations.GraphQLMutation;
+import io.leangen.graphql.annotations.GraphQLNonNull;
+import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.hamsafar.hamsafar.model.Tag;
+import org.hamsafar.hamsafar.repository.TagRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.LinkedHashSet;
+
+@Slf4j
+@Service
+@GraphQLApi
+@CrossOrigin
+@AllArgsConstructor
+public class TagAdjust {
+    private final TagRepository tagRepository;
+
+    @GraphQLMutation
+    public Tag addTag(@GraphQLNonNull String title) {
+        return this.tagRepository.save(Tag.builder()
+                .title(title)
+                .places(new LinkedHashSet<>())
+                .build());
+    }
+}
