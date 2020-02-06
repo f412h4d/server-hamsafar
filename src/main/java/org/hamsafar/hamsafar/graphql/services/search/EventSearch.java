aef06c8ef87659ea5348eb2e99afafdd01bb317c
Farhad Farhadi <farhadf4rh4di@gmail.com>
2020-02-06 16:01:51 +0330

    added search by offset and size for event and place

--- src/main/java/org/hamsafar/hamsafar/graphql/services/search/EventSearch.java
+++ src/main/java/org/hamsafar/hamsafar/graphql/services/search/EventSearch.java
@@ -1,5 +1,6 @@
 package org.hamsafar.hamsafar.graphql.services.search;
 
+import io.leangen.graphql.annotations.GraphQLArgument;
 import io.leangen.graphql.annotations.GraphQLNonNull;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
@@ -10,12 +11,15 @@ import org.hamsafar.hamsafar.model.Tag;
 import org.hamsafar.hamsafar.model.audits.AuditModel;
 import org.hamsafar.hamsafar.repository.EventRepository;
 import org.hamsafar.hamsafar.repository.TagRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Stream;
 
 @Slf4j
 @Service
@@ -29,6 +33,14 @@ public class EventSearch extends AuditModel {
     @GraphQLQuery
     public List<Event> getAllEvents() {
         return this.eventRepository.findAllByVerifiedTrue();
+    }
+
+    @GraphQLQuery
+    public Stream<Event> getAllEventsBySizeAndOffset(@GraphQLArgument(name = "offset", defaultValue = "0", description = "Offset item from beginning of data") int offset,
+                                                     @GraphQLArgument(name = "size", defaultValue = "10", description = "Limit the size of fetched results") int size) {
+        log.error("Got Events By Size");
+        Pageable pageable = PageRequest.of(offset, size);
+        return this.eventRepository.findAll(pageable).get();
     }
 
     @GraphQLQuery
--- src/main/java/org/hamsafar/hamsafar/graphql/services/search/PlaceSearch.java
+++ src/main/java/org/hamsafar/hamsafar/graphql/services/search/PlaceSearch.java
@@ -1,5 +1,6 @@
 package org.hamsafar.hamsafar.graphql.services.search;
 
+import io.leangen.graphql.annotations.GraphQLArgument;
 import io.leangen.graphql.annotations.GraphQLNonNull;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
@@ -9,12 +10,15 @@ import org.hamsafar.hamsafar.model.Place;
 import org.hamsafar.hamsafar.model.Tag;
 import org.hamsafar.hamsafar.repository.PlaceRepository;
 import org.hamsafar.hamsafar.repository.TagRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Stream;
 
 @Slf4j
 @Service
@@ -28,6 +32,14 @@ public class PlaceSearch {
     @GraphQLQuery
     List<Place> getAllPlaces() {
         return this.placeRepository.findAllByVerifiedTrue();
+    }
+
+    @GraphQLQuery
+    public Stream<Place> getAllPlacesBySizeAndOffset(@GraphQLArgument(name = "offset", defaultValue = "0", description = "Offset item from beginning of data") int offset,
+                                                    @GraphQLArgument(name = "size", defaultValue = "10", description = "Limit the size of fetched results") int size) {
+        log.error("Got Places By Size");
+        Pageable pageable = PageRequest.of(offset, size);
+        return this.placeRepository.findAll(pageable).get();
     }
 
     @GraphQLQuery
