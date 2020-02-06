f5eab7a713fe6ecdbcf1a6229e7b942e6fe56285
Farhad Farhadi <farhadf4rh4di@gmail.com>
2020-02-07 03:20:08 +0330

    added isCheckedIn and hadBooked queries

--- src/main/java/org/hamsafar/hamsafar/graphql/services/UserService.java
+++ src/main/java/org/hamsafar/hamsafar/graphql/services/UserService.java
@@ -5,7 +5,11 @@ import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hamsafar.hamsafar.model.Event;
+import org.hamsafar.hamsafar.model.Place;
 import org.hamsafar.hamsafar.model.User;
+import org.hamsafar.hamsafar.repository.EventRepository;
+import org.hamsafar.hamsafar.repository.PlaceRepository;
 import org.hamsafar.hamsafar.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -20,6 +24,8 @@ import java.util.Optional;
 @AllArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PlaceRepository placeRepository;
+    private final EventRepository eventRepository;
 
     @GraphQLQuery
     public List<User> getAllUsers() {
@@ -42,5 +48,69 @@ public class UserService {
             throw new RuntimeException("Invalid User Phone Number, Not Found");
         }
         return optionalUser.get();
+    }
+
+    @GraphQLQuery
+    public User isCheckedInPlace(@GraphQLNonNull String phoneNumber, @GraphQLNonNull String placeId) {
+        Optional<User> optionalUser = this.userRepository.findByPhoneNumberAndVerifiedTrue(phoneNumber);
+        if (optionalUser.isEmpty()) {
+            throw new RuntimeException("Invalid User Phone Number, Not Found");
+        }
+        Optional<Place> optionalPlace = this.placeRepository.findByIdAndVerifiedTrue(placeId);
+        if (optionalPlace.isEmpty()) {
+            throw new RuntimeException("Invalid Place Id, Not Found");
+        }
+        if (optionalUser.get().getCheckedInPlaces().contains(optionalPlace.get())) {
+            return optionalUser.get();
+        }
+        return null;
+    }
+
+    @GraphQLQuery
+    public User isCheckedInEvent(@GraphQLNonNull String phoneNumber, @GraphQLNonNull String eventId) {
+        Optional<User> optionalUser = this.userRepository.findByPhoneNumberAndVerifiedTrue(phoneNumber);
+        if (optionalUser.isEmpty()) {
+            throw new RuntimeException("Invalid User Phone Number, Not Found");
+        }
+        Optional<Event> optionalEvent = this.eventRepository.findByIdAndVerifiedTrue(eventId);
+        if (optionalEvent.isEmpty()) {
+            throw new RuntimeException("Invalid Event Id, Not Found");
+        }
+        if (optionalUser.get().getCheckedInEvents().contains(optionalEvent.get())) {
+            return optionalUser.get();
+        }
+        return null;
+    }
+
+    @GraphQLQuery
+    public User hadBookedPlace(@GraphQLNonNull String phoneNumber, @GraphQLNonNull String placeId) {
+        Optional<User> optionalUser = this.userRepository.findByPhoneNumberAndVerifiedTrue(phoneNumber);
+        if (optionalUser.isEmpty()) {
+            throw new RuntimeException("Invalid User Phone Number, Not Found");
+        }
+        Optional<Place> optionalPlace = this.placeRepository.findByIdAndVerifiedTrue(placeId);
+        if (optionalPlace.isEmpty()) {
+            throw new RuntimeException("Invalid Place Id, Not Found");
+        }
+        if (optionalUser.get().getBookedPlaces().contains(optionalPlace.get())) {
+            return optionalUser.get();
+        }
+        return null;
+    }
+
+    @GraphQLQuery
+    public User hadBookedEvent(@GraphQLNonNull String phoneNumber, @GraphQLNonNull String eventId) {
+        Optional<User> optionalUser = this.userRepository.findByPhoneNumberAndVerifiedTrue(phoneNumber);
+        if (optionalUser.isEmpty()) {
+            throw new RuntimeException("Invalid User Phone Number, Not Found");
+        }
+        Optional<Event> optionalEvent = this.eventRepository.findByIdAndVerifiedTrue(eventId);
+        if (optionalEvent.isEmpty()) {
+            throw new RuntimeException("Invalid Event Id, Not Found");
+        }
+        if (optionalUser.get().getBookedEvents().contains(optionalEvent.get())) {
+            return optionalUser.get();
+        }
+        return null;
     }
 }
